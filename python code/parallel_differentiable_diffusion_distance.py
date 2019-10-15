86827053ea0bb62e977e8de98cb574a4d70fe5f0
OshriHalimi <champ8085@gmail>
2019-10-15 21:10:46 +0300

    added parallel computation for the pseudo-inverse (svd approximation using k eigenfunctions). TODO: parallelize next operations.

--- python code/parallel_differentiable_diffusion_distance.py
+++ python code/parallel_differentiable_diffusion_distance.py
@@ -10,14 +10,13 @@ import visdom
 import time
 from chamfer_python import *
 from utils import *
-from chamfer_python import *
+from utils_parallel import *
 
 # =============PARAMETERS======================================== #
 parser = argparse.ArgumentParser()
 parser.add_argument('--num_evec', type=int, default=10, help='visdom environment')
 parser.add_argument('--nepoch', type=int, default=100000, help='number of epochs to train for')
-parser.add_argument('--full_shape_path', type=str, default="./../data/eigendecomposition/downsampled_tr_reg_000.mat", help='save path')
-parser.add_argument('--partial_shape_path', type=str, default="./../data/faust_range_scans/dataset/tr_reg_004_001.mat", help='save path')
+parser.add_argument('--shape_path', type=str, default="./../data/eigendecomposition/downsampled_tr_reg_004.mat", help='save path')
 parser.add_argument('--save_path', type=str, default='optimization', help='save path')
 parser.add_argument('--env', type=str, default="diffusion", help='visdom environment')
 parser.add_argument('--batchSize', type=int, default=2, help='save path')
@@ -26,7 +25,7 @@ opt = parser.parse_args()
 print(opt)
 
 # =============Loading Intrinsic Properties of GT Completion======================================== #
-x = sio.loadmat(opt.full_shape_path)
+x = sio.loadmat(opt.shape_path)
 adj_VF = torch.from_numpy(x['adj_VF']).cuda().float()
 num_vertices_full = adj_VF.shape[0]
 num_triangles = adj_VF.shape[1]
@@ -42,11 +41,6 @@ evals = torch.from_numpy(x['Lambda']).cuda().double()
 evals = evals[:num_evec].unsqueeze(0).expand(opt.batchSize, num_evec, 1).transpose(2, 1)
 area_V = torch.from_numpy(x['A']).cuda().double().unsqueeze(0).expand(opt.batchSize, num_vertices_full, 1)
 
-# =============Loading======================================== #
-x = sio.loadmat(opt.partial_shape_path)
-part = torch.from_numpy(x['partial_shape'])
-num_vertices_part = part.shape[0]
-part = part.cuda().unsqueeze(0).expand(opt.batchSize, num_vertices_part, 6).double()
 
 # =============DEFINE stuff for logs ======================================== #
 # Launch visdom for visualization
@@ -66,7 +60,7 @@ optimization_loss = []
 
 
 # ===================CREATE optimizer================================= #
-lrate = 0.001 # learning rate
+lrate = 0.01 # learning rate
 pointsReconstructed = torch.randn((2, num_vertices_full, 3), requires_grad=True, device="cuda", dtype = torch.float)
 optimizer = optim.Adam([{'params' : pointsReconstructed}], lr=lrate)
 
@@ -84,7 +78,7 @@ for epoch in range(opt.nepoch):
 
     if epoch == 0:
         pointsReconstructed.data = gt[:,:,:3].data.clone() # initialization of the optimized mesh
-        #pointsReconstructed.data = pointsReconstructed.data + 0.05*torch.randn_like(pointsReconstructed.data)
+        pointsReconstructed.data = pointsReconstructed.data + 0.05*torch.randn_like(pointsReconstructed.data)
 
 
     #Calulcate diffusion distance for the optimized mesh
@@ -92,7 +86,7 @@ for epoch in range(opt.nepoch):
     L_sym = torch.bmm(area_matrix ** 0.5, torch.bmm(L_reconstructed, area_matrix_inv ** 0.5))
     K = torch.tensor(num_evec)
     N = torch.tensor(num_vertices_full)
-    phi_reconstructed_sym, lambda_reconstructed = Eigendecomposition(L_sym[0].cpu().double(), K, N)
+    phi_reconstructed_sym, lambda_reconstructed = EigendecompositionParallel(L_sym[0].cpu().double(), K, N)
     phi_reconstructed_sym = phi_reconstructed_sym.unsqueeze(0).expand(opt.batchSize, num_vertices_full, num_evec).cuda()
 
     phi_reconstructed = torch.bmm(area_matrix_inv ** 0.5, phi_reconstructed_sym) #convert from eigenfunctions of L_sym to L
@@ -103,25 +97,16 @@ for epoch in range(opt.nepoch):
     D_diff_rec = calc_euclidean_dist_matrix(phi_reconstructed)
     loss_diff = torch.mean((D_diff_rec - D_diff_gt) ** 2)
 
-    #Calculate chamfer distance from the partial shape to the optimized mesh
-    d_chamfer = torch.mean(distChamfer(part[:, :, :3], pointsReconstructed.double()) ** 2)
-
-    D_reconstructed = calc_euclidean_dist_matrix(pointsReconstructed).float()
-    D_gt = calc_euclidean_dist_matrix(gt[:, :, :3]).double()
-    loss_euclidean = torch.mean((D_gt < 0.1).double() * (D_reconstructed.double() - D_gt.double()) ** 2)
-
-    loss_net = loss_diff + 100000*d_chamfer + 100*loss_euclidean
+    loss_net = loss_diff
     loss_net.backward()
     optimizer.step()  # gradient update
 
 
     # VIZUALIZE
-    vis.scatter(X=pointsReconstructed[0,:,:3].data.cpu(), win='Optimized mesh',
-                opts=dict(title="Optimized mesh",markersize=2, ), )
-    vis.scatter(X=gt[0,:,:3].data.cpu(), win='full input mesh',
-                opts=dict(title="full input mesh", markersize=2, ), )
-    vis.scatter(X=part[0, :, :3].data.cpu(), win='partial input part',
-                opts=dict(title="partial input part", markersize=2, ), )
+    vis.scatter(X=pointsReconstructed[0,:,:3].data.cpu(), win='Train_output',
+                opts=dict(title="Train_output",markersize=2, ), )
+    vis.scatter(X=gt[0,:,:3].data.cpu(), win='Train_Ground_Truth',
+                opts=dict(title="Train_Ground_Truth", markersize=2, ), )
     save_model = pointsReconstructed.cpu().detach().numpy()
     sio.savemat('optimization_result.mat',
                 {'pointsReconstructed': save_model})
---
+++ python code/utils_parallel.py
@@ -0,0 +1,119 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+from scipy.linalg import eigh
+from scipy.sparse import csr_matrix
+import time
+import scipy
+import scipy.io as sio
+from scipy.sparse import csr_matrix, csc_matrix
+import os
+
+def calculate_grad_input_matrix_parallel(input_matrix, grad_output, row_ind, col_ind, col_ind_mod_N, col_ind_div_N, eigvals, eigvecs, N, K):
+    '''
+    All input variables are torch Tensors
+    :param input_matrix: [N x N]
+    :param input_matrix_support_flatten: vectorization of the rows of the binary support matrix.
+                                         Binary support matrix is defined for input_matrix
+                                         such that there is 1 in the non-zero entries, and 0 otherwise.
+    :param eigvals: [N]
+    :param eigvecs: [N x K]
+    :param K: number of eigenfunctions
+    :param N: number of vertices
+    :return:
+    grad_input_matrix: [N x N] . The gradient with respect to the input matrix.
+    '''
+
+    #approximating pseudo inverse of (lambda_k*Id - input_matrix) for each k, and storing batched pseudo inverse over k
+    grad_input_matrix = csr_matrix((1, N ** 2), dtype=np.double)
+    inv_lambda_mat = eigvals.unsqueeze(1) - eigvals.unsqueeze(0)
+    inv_lambda_mat = 1/inv_lambda_mat
+    ind = np.diag_indices(K)
+    inv_lambda_mat[ind[0], ind[1]] = torch.zeros(K).double()
+    inv_lambda_batch_k = torch.diag_embed(inv_lambda_mat)
+    eigvecs_batch = eigvecs.expand(K, N, K)
+    pinv_approx_batch = torch.bmm(eigvecs_batch, torch.bmm(inv_lambda_batch_k, eigvecs_batch.transpose(2,1)))
+
+    # TODO: parallelize
+    for k in range(K):
+        print(k)
+        P = pinv_approx_batch[k]
+
+        t = time.time()
+        P = csr_matrix(P.cpu().data.numpy())
+        elapsed = time.time() - t
+        print(elapsed)
+
+        uk = eigvecs[:, k].data.numpy()  # dims: n
+
+        t = time.time()
+        data = uk[col_ind_div_N] * np.squeeze(np.asarray(P[row_ind, col_ind_mod_N]))
+        elapsed = time.time() - t
+        print(elapsed)
+        t = time.time()
+        Rk = csr_matrix((data, (row_ind, col_ind)), shape=(N, N ** 2))
+        elapsed = time.time() - t
+        print(elapsed)
+
+        t = time.time()
+        grad_input_matrix = grad_input_matrix + csr_matrix(grad_output[:, k], shape=(1, N)).dot(Rk)
+        elapsed = time.time() - t
+        print(elapsed)
+
+    return grad_input_matrix
+
+
+
+class EigendecompositionParallel(torch.autograd.Function):
+
+    # Note that both forward and backward are @staticmethods
+    @staticmethod
+    def forward(ctx, input_matrix, K, N):
+        input_matrix_np = input_matrix.data.numpy()
+        Knp = K.data.numpy()
+        eigvals, eigvecs = eigh(input_matrix_np, eigvals=(0, Knp - 1), lower=False)
+
+        eigvals = torch.from_numpy(eigvals)
+        eigvecs = torch.from_numpy(eigvecs)
+        ctx.save_for_backward(input_matrix, eigvals, eigvecs, K, N)
+        return (eigvecs, eigvals)
+
+    # This function has only a single output, so it gets only one gradient
+    @staticmethod
+    def backward(ctx, grad_output, grad_output2):
+        input_matrix, eigvals, eigvecs, K, N = ctx.saved_tensors
+        Knp = K.data.numpy().item()
+        Nnp = N.data.numpy().item()
+        grad_K = None
+        grad_N = None
+
+        # constructing the indices for the calculation of sparse du/dL
+        #TODO: refactor this call and the same call in optimizzation script
+        x = sio.loadmat("./../data/eigendecomposition/downsampled_tr_reg_004.mat")
+        adj_VV = x['adj_VV']
+        L_mask_flatten = csc_matrix.reshape(adj_VV, (1, Nnp ** 2))
+        _, col_ind = L_mask_flatten.nonzero()
+        Lnnz = col_ind.shape[0]
+        row_ind = np.arange(Nnp)
+        row_ind = np.repeat(row_ind, Lnnz)
+        col_ind = np.tile(col_ind, Nnp)
+        row_ind = row_ind.astype((np.int64))
+        col_ind = col_ind.astype((np.int64))
+        col_ind_mod_N = col_ind % Nnp
+        col_ind_div_N = col_ind // Nnp
+
+        # TODO: parallelize
+        grad_input_matrix = calculate_grad_input_matrix_parallel(input_matrix, grad_output, row_ind, col_ind, col_ind_mod_N, col_ind_div_N, eigvals, eigvecs, Nnp, Knp)
+        grad_input_matrix = torch.from_numpy(np.transpose(np.reshape(grad_input_matrix.todense(), (Nnp, Nnp))))
+
+        return grad_input_matrix, grad_K, grad_N
+
+
+# Aliasing
+EigendecompositionParallel = EigendecompositionParallel.apply
+
+
+
+
+
+
